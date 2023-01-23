bbb811a7e96761ac7afd834662bdba1168dfce65
kallesiukolaa <kalle.siukolargmail.com>
2023-01-24 00:16:38 +0200

    Initial commit

---
+++ MyDocker/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MyDocker
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, configBuilder) =>
+                {
+                    configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                    configBuilder.AddJsonFile("appsettings.json", optional: true);
+                    configBuilder.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                    configBuilder.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                    configLogging.AddConsole();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Here goes your internal application dependencies
+                    // like EntityFramework context, worker, endpoint, etc.
+                    services.AddScoped<IHostedService, TimedHostedService>();
+                });
+
+            await hostBuilder.RunConsoleAsync();
+        }
+    }
+}
---
+++ MyDocker/TimedHostedService.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class TimedHostedService : IHostedService, IDisposable
+{
+    private readonly ILogger _logger;
+    private Timer _timer;
+
+    public TimedHostedService(ILogger<TimedHostedService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service is starting.");
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero,
+            TimeSpan.FromSeconds(5));
+
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object state)
+    {
+        _logger.LogInformation("Service is running.");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service is stopping.");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
